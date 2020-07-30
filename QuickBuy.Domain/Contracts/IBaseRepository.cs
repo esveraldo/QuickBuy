6f011cffeb79fe6179ce0d49882992e7464147ba
esveraldo <esveraldo@hotmail.com>
2020-07-30 18:04:06 -0300

    Implementando repositorios

---
+++ QuickBuy.Domain/Contracts/IBaseRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Domain.Contracts
+{
+    public interface IBaseRepository<TEntity> : IDisposable where TEntity : class
+    {
+        TEntity ObterPorId(int id);
+        IEnumerable<TEntity> ObterTodos();
+        void Adicionar(TEntity entity);
+        void Atualiza(TEntity entity);
+        void Remover(TEntity entity);
+    }
+}
---
+++ QuickBuy.Repository/Repositories/BaseReporitory.cs
@@ -0,0 +1,44 @@
+﻿using QuickBuy.Domain.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Repository.Repositories
+{
+    public class BaseReporitory<TEntity> : IBaseRepository<TEntity> where TEntity : class
+    {
+        public BaseReporitory()
+        {
+
+        }
+        public void Adicionar(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Atualiza(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity ObterPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remover(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
